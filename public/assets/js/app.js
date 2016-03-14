89d888b357a257ee3b2df18090f1234ebdad3344
Aaron Mazade <ulinaaron@gmail.com>
2016-03-14 02:36:43 -0400

    Re-add the message and AJAX for form

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,3 +9,24 @@ $('.group, .set-item .inner, .header-group').matchHeight();
 $('#navigation').slicknav({
     label: ""
 });
+
+// Contact Form AJAX and Success Message
+
+var formData = $('#contact-form input[name=name]').val();
+
+$('#contact-form').on('submit', function(e) {
+    e.preventDefault();
+    var action = $(this).attr('action');
+    var data = $(this).serialize();
+    var callback = function(data) {
+        $(this).parent().append().html('<div class="message success">I will be in touch with you shortly.</div>');
+    };
+    
+    // Process the form
+    $.ajax({
+        type: 'POST',
+        url: action,
+        data: data,
+        sucess: callback
+    });
+});

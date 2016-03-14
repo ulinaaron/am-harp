9b7e7a1ed0918449e13306e1fb1a65848fdd30c3
Aaron Mazade <ulinaaron@gmail.com>
2016-03-14 02:45:48 -0400

    trying more

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -16,17 +16,14 @@ var formData = $('#contact-form input[name=name]').val();
 
 $('#contact-form').on('submit', function(e) {
     e.preventDefault();
-    var action = $(this).attr('action');
-    var data = $(this).serialize();
-    var callback = function() {
-        $(this).parent().append().html('<div class="message success">I will be in touch with you shortly.</div>');
-    };
     
     // Process the form
     $.ajax({
         type: 'POST',
-        url: action,
-        data: data,
-        success: callback
+        url: $(this).attr('action'),
+        data: $(this).serialize(),
+        success: function(data) {
+            $(this).parent().append().html('<div class="message success">I will be in touch with you shortly.</div>');
+        }
     });
 });

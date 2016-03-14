2ac9f7d8c2e619282c64616ff3bd51b845efcbdd
Aaron Mazade <ulinaaron@gmail.com>
2016-03-14 00:42:50 -0400

    build

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -2,12 +2,31 @@
  * Assemble the libraries!
  */
 
-(function() {
-	$(function() {
-	    $('.group, .set-item .inner, .header-group').matchHeight();
-	});
+// Add matchHeight to classes
+$('.group, .set-item .inner, .header-group').matchHeight();
+
+// Start SlickNav
+$('#navigation').slicknav({
+    label: ""
+});
+
+// Contact Form AJAX and Success Message
+
+$('#contact-form').on('submit', function(e) {
+    e.preventDefault();
+    var action = $(this).attr('action');
+    var data = $(this).serialize();
+    var callback = function(data) {
+        $(this).remove();
+        
+        $(this).text('Thank you!');
+    };
     
-    $('#navigation').slicknav({
-		label: ""
-	});
-})(jQuery);
+    // Process the form
+    $.ajax({
+        type: 'POST',
+        url: action,
+        data: data,
+        sucess: callback
+    });
+});

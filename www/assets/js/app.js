31c7e1da159ac094709a8a239b11c2184b0e78d2
Aaron Mazade <ulinaaron@gmail.com>
2016-03-14 01:15:27 -0400

    Remove form AJAX

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -9,24 +9,3 @@ $('.group, .set-item .inner, .header-group').matchHeight();
 $('#navigation').slicknav({
     label: ""
 });
-
-// Contact Form AJAX and Success Message
-
-$('#contact-form').on('submit', function(e) {
-    e.preventDefault();
-    var action = $(this).attr('action');
-    var data = $(this).serialize();
-    var name = $(this).find('input[name="name"]');
-    var callback = function(data) {
-        $(this).parent().find('.group-title').text('Thanks, ' + name + '!');
-        $(this).append().html('<div class="message success">I will be in touch with you shortly.</div>');
-    };
-    
-    // Process the form
-    $.ajax({
-        type: 'POST',
-        url: action,
-        data: data,
-        sucess: callback
-    });
-});

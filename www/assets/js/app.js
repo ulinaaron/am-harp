a2d2842d3d9d308de05db878a12c5cc9bbfd9dec
Aaron Mazade <ulinaaron@gmail.com>
2016-03-15 16:34:28 -0400

    Update app.js

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -12,24 +12,3 @@ $('#navigation').slicknav({
   label: '';
 });
 
-// Contact Form AJAX and Success Message
-
-var $contactForm = $('#contact-form');
-
-$contactForm.submit(function(e) {
-  e.preventDefault();
-   
-  // Process the form
-  $.ajax({
-    type: 'POST',
-    url: $contactForm.attr('action'),
-    accept: {
-    javascript: 'application/javascript'
-  },
-    data: $contactForm.serialize()
-  }).done(function(data) {
-    $contactForm.parent().find('.group-title').text('Thank You!').fadeIn('slow');
-    $contactForm.parent().append('<div class="message success">I will be in touch with you shortly.</div>').fadeIn('slow');
-    $contactForm.remove();
-  });
-});

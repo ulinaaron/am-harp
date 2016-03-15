5f39e24b619f518634da0d06e4549216d9521375
Aaron Mazade <ulinaaron@gmail.com>
2016-03-15 11:17:27 -0400

    JSCS fixes

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -7,27 +7,27 @@ $('.group, .set-item .inner, .header-group').matchHeight();
 
 // Start SlickNav
 $('#navigation').slicknav({
-    label: ""
+  label: '';
 });
 
 // Contact Form AJAX and Success Message
 
 var $contactForm = $('#contact-form');
 
-$contactForm.submit( function(e) {
-    e.preventDefault();
-    
-    // Process the form
-    $.ajax({
-        type: 'POST',
-        url: $contactForm.attr('action'),
-        accept: {
-            javascript: 'application/javascript'
-        },
-        data: $contactForm.serialize()
-    }).done( function(data) {
-        $contactForm.parent().find('.group-title').text('Thank You!').fadeIn('slow');
-        $contactForm.parent().append('<div class="message success">I will be in touch with you shortly.</div>').fadeIn('slow');
-        $contactForm.remove();
-    });
+$contactForm.submit(function(e) {
+  e.preventDefault();
+   
+  // Process the form
+  $.ajax({
+    type: 'POST',
+    url: $contactForm.attr('action'),
+    accept: {
+    javascript: 'application/javascript'
+  },
+    data: $contactForm.serialize()
+  }).done(function(data) {
+    $contactForm.parent().find('.group-title').text('Thank You!').fadeIn('slow');
+    $contactForm.parent().append('<div class="message success">I will be in touch with you shortly.</div>').fadeIn('slow');
+    $contactForm.remove();
+  });
 });

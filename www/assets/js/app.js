9aae244e335d3f0f846ea86c2fe69237ecf0be80
Aaron Mazade <ulinaaron@gmail.com>
2016-05-15 04:56:27 -0400

    compile

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -7,7 +7,7 @@ $('.group, .set-item .inner, .header-group').matchHeight();
 
 // Start SlickNav
 $('#navigation').slicknav({
-  label: ''
+  label: 'Menu'
 });
 
 // Contact Form AJAX and Success Message
